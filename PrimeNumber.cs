d7122e236c4e4ae65212d01f4b426d2efa66c492
jayukasode23 <jayukasode2398@gmail.com>
2022-10-07 17:15:29 +0530

    Added Prime Number

---
+++ PrimeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void CheckPrimeNumber()
+        {
+            int num = 7;
+            int count = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("Is Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Is Not Prime Number");
+            }
+        }
+    }
+}
