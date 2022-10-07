eb62bf386e52eee1a2ed54509b7f21cc01a90421
jayukasode23 <jayukasode2398@gmail.com>
2022-10-07 17:16:37 +0530

    Added Reverse Number

---
+++ ReverseNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        public static void CheckReverseNumber()
+        {
+            int n, reverse = 0, rem;
+            Console.WriteLine("Enter Number : ");
+            n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n = n / 10;
+            }
+            Console.WriteLine("Reversed Number : " + reverse);
+        }
+    }
+}
