233367a4a23a8ceaf205041c07bd93a3222abd61
jayukasode23 <jayukasode2398@gmail.com>
2022-10-07 17:12:34 +0530

    Added Fibonacci Series

---
+++ FibonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibonacciSeries
+    {
+        public static void CalculateFibonacciSeries()
+        {
+            int number, n1 = 0, n2 = 1, n3, i;
+
+            Console.WriteLine("Enter the number of elements");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (i = 2; i <= number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+
+        }
+    }
+}
